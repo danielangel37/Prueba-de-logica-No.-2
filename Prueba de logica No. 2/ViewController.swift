9e15c56350aa11137b39d79732d3f7c27b8d63cb
Daniel Angel <daniel.danielangel@hotmail.com>
2023-03-20 22:05:31 -0600

    Initial Commit

---
+++ Prueba de logica No. 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prueba de logica No. 2
+//
+//  Created by Daniel Angel on 20/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
